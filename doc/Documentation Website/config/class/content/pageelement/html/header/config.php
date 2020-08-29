af5a3fc5d9af0c20883cc6c2b76d7cd66fb6e75d
gugus2000 <gugus2000@protonmail.com>
2020-08-29 18:13:02 +0200

    update doc

---
+++ doc/Documentation Website/config/class/content/pageelement/html/header/config.php
@@ -0,0 +1,18 @@
+<?php
+
+/* [LINKS] */
+/* Home */
+$GLOBALS['config']['class']['content']['pageelement']['html']['header']['links']['home']=array('application' => 'doc', 'action' => 'home');
+/* Getting started */
+$GLOBALS['config']['class']['content']['pageelement']['html']['header']['links']['getting_started']=array('application' => 'doc', 'action' => 'getting_started');
+/* API */
+$GLOBALS['config']['class']['content']['pageelement']['html']['header']['links']['api']=array('application' => 'doc', 'action' => 'api');
+/* BOUML */
+$GLOBALS['config']['class']['content']['pageelement']['html']['header']['links']['bouml']=array('application' => 'doc', 'action' => 'bouml');
+/* Search */
+$GLOBALS['config']['class']['content']['pageelement']['html']['header']['links']['search']=array('application' => 'doc', 'action' => 'search');
+/* FAQ */
+$GLOBALS['config']['class']['content']['pageelement']['html']['header']['links']['faq']=array('application' => 'doc', 'action' => 'faq');
+
+
+?>
---
+++ doc/Documentation Website/lang/class/content/pageelement/html/header/FR.lang.php
@@ -0,0 +1,44 @@
+<?php
+
+/* Title displayed in the header */
+$GLOBALS['lang']['class']['content']['pageelement']['html']['header']['title']='Documentation';
+
+/* [LINKS] */
+/* [FIRST] */
+/* Title of the link */
+$GLOBALS['lang']['class']['content']['pageelement']['html']['header']['first']['title']='Revenir en haut';
+/* Value displayed in the link */
+$GLOBALS['lang']['class']['content']['pageelement']['html']['header']['first']['link']='Haut';
+/* [PAGE] */
+/* Home */
+/* Title of the link */
+$GLOBALS['lang']['class']['content']['pageelement']['html']['header']['page']['home']['title']='Accueil de la documentation';
+/* Value displayed in the link */
+$GLOBALS['lang']['class']['content']['pageelement']['html']['header']['page']['home']['link']='Accueil';
+/* Getting started */
+/* Title of the link */
+$GLOBALS['lang']['class']['content']['pageelement']['html']['header']['page']['getting_started']['title']='Pour commencer avec PHosPhore';
+/* Value displayed in the link */
+$GLOBALS['lang']['class']['content']['pageelement']['html']['header']['page']['getting_started']['link']='Pour commencer';
+/* API */
+/* Title of the link */
+$GLOBALS['lang']['class']['content']['pageelement']['html']['header']['page']['api']['title']='Documentation de l\'API';
+/* Value displayed in the link */
+$GLOBALS['lang']['class']['content']['pageelement']['html']['header']['page']['api']['link']='API';
+/* BOUML */
+/* Title of the link */
+$GLOBALS['lang']['class']['content']['pageelement']['html']['header']['page']['bouml']['title']='Indication sur l\'usage de la documentation généré par BOUML';
+/* Value displayed in the link */
+$GLOBALS['lang']['class']['content']['pageelement']['html']['header']['page']['bouml']['link']='BOUML';
+/* Search */
+/* Title of the link */
+$GLOBALS['lang']['class']['content']['pageelement']['html']['header']['page']['search']['title']='Recherche dans la documentation de PHosPhore';
+/* Value displayed in the link */
+$GLOBALS['lang']['class']['content']['pageelement']['html']['header']['page']['search']['link']='Recherche';
+/* FAQ */
+/* Title of the link */
+$GLOBALS['lang']['class']['content']['pageelement']['html']['header']['page']['faq']['title']='FAQ de PHosPhore';
+/* Value displayed in the link */
+$GLOBALS['lang']['class']['content']['pageelement']['html']['header']['page']['faq']['link']='FAQ';
+
+?>
