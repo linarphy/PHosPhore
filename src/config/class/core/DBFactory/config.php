1b68a6b648cd467656da3cf4236cd6a3ac9bf8ed
gugus2000 <gugus2000@protonmail.com>
2021-11-17 16:12:16 +0100

    removed personnal configuration

---
+++ src/config/class/core/DBFactory/config.php
@@ -0,0 +1,18 @@
+<?php
+
+/** [MysqlConnection] **/
+/** [database] **/
+/* name of the database */
+$GLOBALS['config']['class']['core']['DBFactory']['database']['name'] = 'website';
+/* host which possess the database */
+$GLOBALS['config']['class']['core']['DBFactory']['database']['host'] = 'localhost';
+/* name of the user which will connect to the database */
+$GLOBALS['config']['class']['core']['DBFactory']['database']['username'] = 'root';
+/* password of the user which will connect to the database */
+$GLOBALS['config']['class']['core']['DBFactory']['database']['password'] = '';
+/* specifics options used to connect to the database (see https://php.net/pdo.construct) */
+$GLOBALS['config']['class']['core']['DBFactory']['database']['options'] = null;
+/** [/database] **/
+/** [/MysqlConnection] **/
+
+?>
