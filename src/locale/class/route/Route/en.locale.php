ebdd57dde237e87db99816117eda99a2729387bd
gugus2000 <gugus2000@protonmail.com>
2022-04-20 03:01:18 +0200

    Add locale file:
    for parameter management

---
+++ src/locale/class/route/Route/en.locale.php
@@ -0,0 +1,17 @@
+<?php
+
+/** [createLinkGet] **/
+/* unknown route type */
+$GLOBALS['locale']['class']['route']['Router']['createLinkGet']['unknown_type'] = 'this route is neither a folder nor a page';
+/** [/createLinkGet] **/
+/** [createLinkMixed] **/
+/* unknown route type */
+$GLOBALS['locale']['class']['route']['Router']['createLinkMixed']['unknown_type'] = 'this route in neither a folder nor a page';
+/** [/createLinkMixed] **/
+/** [createLinkRoute] **/
+/* unknown route type */
+$GLOBALS['locale']['class']['route']['Router']['createLinkRoute']['unknown_type'] = 'this route is neither a folder nor a page';
+/** [/createLinkRoute] **/
+
+
+?>
