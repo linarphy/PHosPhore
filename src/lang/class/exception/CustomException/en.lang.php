abec1e1b91fc0a67a3bc3c6baf5c3d20bdf150f3
linarphy <gugus2000@courrier.dev>
2022-11-14 01:23:25 +0100

    Update translation for \exception\CustomException

---
+++ src/lang/class/exception/CustomException/en.lang.php
@@ -0,0 +1,10 @@
+<?php
+
+/** [default] **/
+/** [message] **/
+/* default exception message */
+$GLOBALS['locale']['class']['exception']['customException']['default']['message'] = 'Unknow error, cannot construct the exception';
+/** [/message] **/
+/** [/default] **/
+
+?>
---
+++ src/locale/class/exception/CustomException/en.locale.php
@@ -0,0 +1,10 @@
+<?php
+
+/** [default] **/
+/** [message] **/
+/* default exception notification message */
+$GLOBALS['locale']['class']['exception']['customException']['default']['message'] = 'Sorry, an unknown error happened';
+/** [/message] **/
+/** [/default] **/
+
+?>
