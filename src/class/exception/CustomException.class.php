717e19d373a6f3259a18f79f032305408ba82f48
linarphy <gugus2000@courrier.dev>
2022-11-14 01:08:34 +0100

    Update \exception\CustomException
    Add tokens arg to add dynamic log

--- src/class/exception/CustomException.class.php
+++ src/class/exception/CustomException.class.php
@@ -4,15 +4,65 @@ namespace exception;
 
 abstract class CustomException extends \Exception
 {
-	public function __construct(?string $message = null, int $code = 0, ?\Throwable $previous = null)
+	/**
+	 * default logger types
+	 *
+	 * @var array
+	 */
+	const LOGGER_TYPES = [];
+	/**
+	 * CustomException constructor
+	 *
+	 * @param ?string $message error message (default to null)
+	 *
+	 * @param int $code user defined code (default to 0)
+	 *
+	 * @param ?\Throwable $previous previous throwable (default to null)
+	 *
+	 * @param ?array $logger_types types of the generated log (default to null)
+	 *
+	 * @param ?\user\Notification $notification if set, notification to display
+	 */
+	public function __construct(?string $message = null, int $code = 0, ?\Throwable $previous = null, array $logger_types = null, ?\user\Notification $notification = null, ?array $tokens = [])
 	{
 		if ($message === null)
 		{
-			throw new $this('Unknown ' . \get_class($this));
+			throw new $this(
+				$GLOBALS['lang']['class']['exception']['CustomException']['default']['message'],
+				$code,
+				$previous,
+				$logger_types,
+				$notification,
+				$tokens,
+			);
 		}
-		parent::__construct($message, $code, $previous);
+		try
+		{
+			if ($logger_types === null)
+			{
+				$logger_types = $this::LOGGER_TYPES;
+			}
+			$GLOBALS['Logger']->log($logger_types, $message, $tokens);
+			if ($notification != null)
+			{
+				if ($notification->get('content') === null)
+				{
+					$notification->set('content', $GLOBALS['locale']['class']['exception']['CustomException']['default']['message']);
+				}
+				$notification->addToSession();
+			}
+			parent::__construct($message, $code, $previous);
+		}
+		catch (\Throwable $exception)
+		{
+			parent::__construct($GLOBALS['lang']['class']['exception']['CustomException']['default']['message']);
+		}
 	}
-
+	/**
+	 * convert this exception into a readable string
+	 *
+	 * @return string
+	 */
 	public function __toString()
 	{
 		return \htmlspecialchars(\get_class($this)) . ' ' . \htmlspecialchars($this->message) . ' in ' . \htmlspecialchars($this->file) . '(' . \htmlspecialchars($this->line) . ')\n' . \htmlspecialchars($this->getTraceAsString());
