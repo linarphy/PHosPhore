94916272807f89d8c70e38c27a77df37979d4afd
linarphy <linarphy@courrier.dev>
2023-02-27 00:41:36 +0100

    Update formatting config pageelement

--- src/config/class/content/pageelement/config.php
+++ src/config/class/content/pageelement/config.php
@@ -1,28 +1,40 @@
 <?php
 
+$CONFIG =& $GLOBALS
+           ['config']
+           ['class']
+           ['content']
+           ['pageelement'];
+
 /** [preset] **/
 /* about pre-existing page preset to construct a Page */
 /* default preset */
-$GLOBALS['config']['class']['content']['pageelement']['preset']['default_preset'] = 'none';
+$CONFIG['preset']['default_preset'] = 'none';
 /* notifications element name in main content */
-$GLOBALS['config']['class']['content']['pageelement']['preset']['notification_element_name'] = 'notifications';
+$CONFIG['preset']['notification_element_name'] = 'notifications';
 /* template directory for preset */
-$GLOBALS['config']['class']['content']['pageelement']['preset']['template_folder'] = 'preset';
+$CONFIG['preset']['template_folder'] = 'preset';
 /** [list] **/
 /* list of vanilla available preset */
 /** [default_html] **/
 /* default html page */
 /* \content\pageelement\PageElement associated to the main content */
-$GLOBALS['config']['class']['content']['pageelement']['preset']['list']['default_html']['page_element'] = '\content\pageelement\preset\default\html\PageElement';
-/* \content\pageelement\PageElement associated to the notification content */
-$GLOBALS['config']['class']['content']['pageelement']['preset']['list']['default_html']['notification_element'] = '\content\pageelement\preset\default\html\NotificationElement';
+$CONFIG['preset']['list']['default_html']
+['page_element'] = '\content\pageelement\preset\default\html\PageElement';
+/* \content\pageelement\PageElement associated to the notification
+ * content */
+$CONFIG['preset']['list']['default_html']
+['notification_element'] = '\content\pageelement\preset\default\html\NotificationElement';
 /** [/default_html] **/
 /** [none] **/
 /* no preset */
 /* \content\pageelement\PageElement associated to the main content */
-$GLOBALS['config']['class']['content']['pageelement']['preset']['list']['none']['page_element'] = '\content\pageelement\PageElement';
-/* \content\pageelement\PageElement associated to the notification content */
-$GLOBALS['config']['class']['content']['pageelement']['preset']['list']['none']['notification_element'] = '\content\pageelement\PageElement';
+$CONFIG['preset']['list']['none']
+['page_element'] = '\content\pageelement\PageElement';
+/* \content\pageelement\PageElement associated to the notification
+ * content */
+$CONFIG['preset']['list']['none']
+['notification_element'] = '\content\pageelement\PageElement';
 /** [/none] **/
 /** [/list] **/
 /** [/preset] **/
