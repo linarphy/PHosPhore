0fdf9f6da434aac828fdb10814cca4af482b4960
linarphy <linarphy@courrier.dev>
2023-02-27 00:13:05 +0100

    Update formatting for lang & locale (Base)

--- src/lang/class/core/Base/en.lang.php
+++ src/lang/class/core/Base/en.lang.php
@@ -1,127 +1,186 @@
 <?php
 
+$LANG =& $GLOBALS
+         ['lang']
+         ['class']
+         ['core']
+         ['Base'];
+
 /** [__construct] **/
 /* construct the object */
-$GLOBALS['lang']['class']['core']['Base']['__construct']['start'] = 'constructing a {class} instance';
+$LANG['__construct']['start'] = 'constructing a {class} instance';
 /* cannot hydrate the object */
-$GLOBALS['lang']['class']['core']['Base']['__construct']['error_hydrate'] = 'cannot hydrate the object {class}: {exception}';
+$LANG['__construct']['error_hydrate'] = 'cannot hydrate the object 
+{class}: {exception}';
 /* cannot construct the object */
-$GLOBALS['lang']['class']['core']['Base']['__construct']['error'] = 'cannot construct a {class} instance: {exception}';
+$LANG['__construct']['error'] = 'cannot construct a {class} instance: 
+{exception}';
 /** [/__construct] **/
 
 /** [clone] **/
 /* cloning the object */
-$GLOBALS['lang']['class']['core']['Base']['clone']['start'] = 'cloning the object {class}';
+$LANG['clone']['start'] = 'cloning the object {class}';
 /* cannot convert this object into an array */
-$GLOBALS['lang']['class']['core']['Base']['clone']['error_table'] = 'cannot convert the object {class} into a table: {exception}';
+$LANG['clone']['error_table'] = 'cannot convert the object {class} into 
+a table: {exception}';
 /* error during object cloning */
-$GLOBALS['lang']['class']['core']['Base']['clone']['error'] = 'an error occured when cloning the object {class}: {exception}';
+$LANG['clone']['error'] = 'an error occured when cloning the object 
+{class}: {exception}';
 /** [/clone] **/
 
 /** [display] **/
 /* start the process */
-$GLOBALS['lang']['class']['core']['Base']['display']['start'] = 'start to display {attribute} of {class}';
+$LANG['display']['start'] = 'start to display {attribute} of {class}';
 /* cannot convert this object in array */
-$GLOBALS['lang']['class']['core']['Base']['display']['error_table'] = 'cannot convert the object {class} into an array: {exception}';
+$LANG['display']['error_table'] = 'cannot convert the object {class} 
+into an array: {exception}';
 /* error in \phosphore_display */
-$GLOBALS['lang']['class']['core']['Base']['display']['error_display'] = 'cannot display the converted array from {class} into a string: {exception}';
+$LANG['display']['error_display'] = 'cannot display the converted array 
+from {class} into a string: {exception}';
 /* display the entire object */
-$GLOBALS['lang']['class']['core']['Base']['display']['object'] = 'display the entire object {class}';
+$LANG['display']['object'] = 'display the entire object {class}';
 /* the attribute does not exist */
-$GLOBALS['lang']['class']['core']['Base']['display']['undefined'] = 'cannot display the attribute {attribute} of {class} because it does not exist';
+$LANG['display']['undefined'] = 'cannot display the attribute 
+{attribute} of {class} because it does not exist';
 /* cannot get display method for this attribute */
-$GLOBALS['lang']['class']['core']['Base']['display']['error_getDisp'] = 'cannot get the method to display the attribute {attribute} from the class {class}: {exception}';
+$LANG['display']['error_getDisp'] = 'cannot get the method to display 
+the attribute {attribute} from the class {class}: {exception}';
 /* the method to display this attribute exist */
-$GLOBALS['lang']['class']['core']['Base']['display']['exist'] = 'custom method {method} used to display the attribute {attribute} of {class}';
+$LANG['display']['exist'] = 'custom method {method} used to display 
+the attribute {attribute} of {class}';
 /* the custom method to display this attribute threw an error */
-$GLOBALS['lang']['class']['core']['Base']['display']['custom_method_error'] = 'an error occured in the custom method {method} used to display the attribute {attribute} of class {class}: {exception}';
+$LANG['display']['custom_method_error'] = 'an error occured in the 
+custom method {method} used to display the attribute {attribute} of 
+class {class}: {exception}';
 /* cannot get the attribute value */
-$GLOBALS['lang']['class']['core']['Base']['display']['get_attribute'] = 'cannot get the value of the attribute {attribute} from the class {class}: {exception}';
+$LANG['display']['get_attribute'] = 'cannot get the value of the 
+attribute {attribute} from the class {class}: {exception}';
 /* the method to display this attribute does not exist */
-$GLOBALS['lang']['class']['core']['Base']['display']['not_exist'] = 'no custom method {method} found, default method used to display the attribute {attribute} of {class}';
-/* error in \phosphore_display when displaying the attribute value (not itself) */
-$GLOBALS['lang']['class']['core']['Base']['display']['error_display_attribute'] = 'cannot display the attribute {attribue} from {class}: {exception}';
+$LANG['display']['not_exist'] = 'no custom method {method} found, 
+default method used to display the attribute {attribute} of {class}';
+/* error in \phosphore_display when displaying the attribute value (not
+ * itself) */
+$LANG['display']['error_display_attribute'] = 'cannot display the 
+attribute {attribue} from {class}: {exception}';
 /* an error occured when displaying an attribute */
-$GLOBALS['lang']['class']['core']['Base']['display']['error'] = 'an error occured and the element {attribute} of the object {class} could not be displayed: {exception}';
+$LANG['display']['error'] = 'an error occured and the element 
+{attribute} of the object {class} could not be displayed: {exception}';
 /** [/display] **/
 
 /** [get] **/
 /* start of the process */
-$GLOBALS['lang']['class']['core']['Base']['get']['start'] = 'trying to get the attribute {attribute} of the object {class}';
+$LANG['get']['start'] = 'trying to get the attribute {attribute} of the 
+object {class}';
 /* attribute not defined */
-$GLOBALS['lang']['class']['core']['Base']['get']['not_defined'] = 'cannot get the attribute {attribute} of {class} because it does not exist';
+$LANG['get']['not_defined'] = 'cannot get the attribute {attribute} of 
+{class} because it does not exist';
 /* getGet threw an exception */
-$GLOBALS['lang']['class']['core']['Base']['get']['error_getGet'] = 'an error occured when getting name of the getter method for the attribute {attribute} from the class {class}: {exception}';
+$LANG['get']['error_getGet'] = 'an error occured when getting name of 
+the getter method for the attribute {attribute} from the class {class}: 
+{exception}';
 /* the custom method to get this attribute threw an error */
-$GLOBALS['lang']['class']['core']['Base']['get']['error_custom_method'] = 'an error occured in the custom method {method} used to get the attribut {attribute} of class {class}: {exception}';
+$LANG['get']['error_custom_method'] = 'an error occured in the custom 
+method {method} used to get the attribut {attribute} of class {class}: 
+{exception}';
 /* custom method exist */
-$GLOBALS['lang']['class']['core']['Base']['get']['getter'] = 'custom method {method} used to access the attribute {attribute} of {class}';
+$LANG['get']['getter'] = 'custom method {method} used to access the 
+attribute {attribute} of {class}';
 /* no custom method */
-$GLOBALS['lang']['class']['core']['Base']['get']['default'] = 'no custom method {method} found, default method used to access the attribute {attribute} of {class}';
+$LANG['get']['default'] = 'no custom method {method} found, default 
+method used to access the attribute {attribute} of {class}';
 /* cannot get the attribute of this object */
-$GLOBALS['lang']['class']['core']['Base']['get']['error'] = 'cannot get the attribute {attribute} of the class {class}: {exception}';
+$LANG['get']['error'] = 'cannot get the attribute {attribute} of the 
+class {class}: {exception}';
 /** [/get] **/
 
 /** [getDisp] **/
 /* empty attribute */
-$GLOBALS['lang']['class']['core']['Base']['getDisp']['empty'] = 'cannot get the displayer of attribute {attribute} which is empty for {class}';
-/* an error occured when getting the displayer of an attribute for this object */
-$GLOBALS['lang']['class']['core']['Base']['getDisp']['empty'] = 'cannot get the displayer of the attribute {attribute} of the class {class}: {exception}';
+$LANG['getDisp']['empty'] = 'cannot get the displayer of attribute 
+{attribute} which is empty for {class}';
+/* an error occured when getting the displayer of an attribute for this
+ * object */
+$LANG['getDisp']['empty'] = 'cannot get the displayer of the attribute 
+{attribute} of the class {class}: {exception}';
 /** [/getDisp] **/
 
 /** [getGet] **/
 /* empty attribute */
-$GLOBALS['lang']['class']['core']['Base']['getGet']['empty'] = 'cannot get the getter of an attribute {attribute} which is empty for {class}';
+$LANG['getGet']['empty'] = 'cannot get the getter of an attribute 
+{attribute} which is empty for {class}';
 /* special attribute */
-$GLOBALS['lang']['class']['core']['Base']['getGet']['special'] = 'the method for the attribute {attribute} for {class} is already used for something else, the getter name associated to it is different: {method}';
-/* an error occured when getting the getter of the attribute of this object */
-$GLOBALS['lang']['class']['core']['Base']['getGet']['error'] = 'cannot get the getter of the attribute {attribute} of class {class}: {exception}';
+$LANG['getGet']['special'] = 'the method for the attribute {attribute} 
+for {class} is already used for something else, the getter name 
+associated to it is different: {method}';
+/* an error occured when getting the getter of the attribute of this
+ * object */
+$LANG['getGet']['error'] = 'cannot get the getter of the attribute 
+{attribute} of class {class}: {exception}';
 /** [/getGet] **/
 
 /** [getSet] **/
 /* empty attribute */
-$GLOBALS['lang']['class']['core']['Base']['getSet']['empty'] = 'cannot get the setter of an attribute {attribute} wich is empty for {class}';
-/* an error occured when getting the setter of an attribute of this object */
-$GLOBALS['lang']['class']['core']['Base']['getSet']['error'] = 'cannot get the setter of the attribute {attribute} of class {class}: {exception}';
+$LANG['getSet']['empty'] = 'cannot get the setter of an attribute 
+{attribute} wich is empty for {class}';
+/* an error occured when getting the setter of an attribute of this
+ * object */
+$LANG['getSet']['error'] = 'cannot get the setter of the attribute 
+{attribute} of class {class}: {exception}';
 /** [/getSet] **/
 
 /** [hydrate] **/
 /* start hydrating */
-$GLOBALS['lang']['class']['core']['Base']['hydrate']['start'] = 'start hydratation of {class}';
+$LANG['hydrate']['start'] = 'start hydratation of {class}';
 /* the attribute is not a string */
-$GLOBALS['lang']['class']['core']['Base']['hydrate']['type_attribute'] = 'an attribute of {class} should be a string, {type} given';
+$LANG['hydrate']['type_attribute'] = 'an attribute of {class} should be 
+a string, {type} given';
 /* error when setting a value to the attribute */
-$GLOBALS['lang']['class']['core']['Base']['hydrate']['error_set'] = 'cannot set a value to the attribute {attribute} of class {class}: {exception}';
+$LANG['hydrate']['error_set'] = 'cannot set a value to the attribute 
+{attribute} of class {class}: {exception}';
 /* end hydrating */
-$GLOBALS['lang']['class']['core']['Base']['hydrate']['end'] = 'hydratation of {class} finished, {count} attributes stored';
+$LANG['hydrate']['end'] = 'hydratation of {class} finished, {count} 
+attributes stored';
 /* error when hydrating an object */
-$GLOBALS['lang']['class']['core']['Base']['hydrate']['error'] = 'cannot hydrate an object of the class {class}: {exception}';
+$LANG['hydrate']['error'] = 'cannot hydrate an object of the class 
+{class}: {exception}';
 /** [/hydrate] **/
 
 /** [set] **/
 /* start the process */
-$GLOBALS['lang']['class']['core']['Base']['set']['start'] = 'start to set a value for the attribute {attribute} of {class}';
+$LANG['set']['start'] = 'start to set a value for the attribute 
+{attribute} of {class}';
 /* attribute does not exist */
-$GLOBALS['lang']['class']['core']['Base']['set']['undefined'] = 'cannot set a value for the attribute {attribute} of {class} because this attribute does not exist in this class';
+$LANG['set']['undefined'] = 'cannot set a value for the attribute 
+{attribute} of {class} because this attribute does not exist in this 
+class';
 /* error when generating setter name for the attribute of this object */
-$GLOBALS['lang']['class']['core']['Base']['set']['error_getSet'] = 'error occured in generating setter name for the attribute {attribute} of {class}: {exception}';
+$LANG['set']['error_getSet'] = 'error occured in generating setter name 
+for the attribute {attribute} of {class}: {exception}';
 /* custom method exists */
-$GLOBALS['lang']['class']['core']['Base']['set']['custom_method'] = 'custom setter found: {method} , defining {attribute} for {class}';
+$LANG['set']['custom_method'] = 'custom setter found: {method} , 
+defining {attribute} for {class}';
 /* custom method threw an error */
-$GLOBALS['lang']['class']['core']['Base']['set']['error_custom_method'] = 'an error occured in teh custom method {method} for setting the attribute {attribute} of {class}: {exception}';
+$LANG['set']['error_custom_method'] = 'an error occured in the custom 
+method {method} for setting the attribute {attribute} of {class}: 
+{exception}';
 /* no custom method */
-$GLOBALS['lang']['class']['core']['Base']['set']['default_method'] = 'custom setter not found, defining {attribute} for {class} with default method';
+$LANG['set']['default_method'] = 'custom setter not found, defining 
+{attribute} for {class} with default method';
 /* error when setting an attribute */
-$GLOBALS['lang']['class']['core']['Base']['set']['error'] = 'cannot set a valur for the attribute {attribute} of {class}: {exception}';
+$LANG['set']['error'] = 'cannot set a valur for the attribute 
+{attribute} of {class}: {exception}';
 /** [/set] **/
 
 /** [table] **/
 /* start the process */
-$GLOBALS['lang']['class']['core']['Base']['table']['start'] = 'start to convert {class} into an array (depth: {depth})';
+$LANG['table']['start'] = 'start to convert {class} into an array 
+(depth: {depth})';
 /* depth value not accepted */
-$GLOBALS['lang']['class']['core']['Base']['table']['undefined_depth'] = 'depth {depth} is not valid for converting {class} into an array';
+$LANG['table']['undefined_depth'] = 'depth {depth} is not valid for 
+converting {class} into an array';
 /* an error occured when converting the object into a table */
-$GLOBALS['lang']['class']['core']['Base']['table']['error'] = 'an error occured when converting the object into a table with de depth {depth}, with the mode strict {strict} for the class {class}: {exception}';
+$LANG['table']['error'] = 'an error occured when converting the object 
+into a table with de depth {depth}, with the mode strict {strict} for 
+the class {class}: {exception}';
 /** [/table] **/
 
 ?>
--- src/locale/class/core/Base/en.locale.php
+++ src/locale/class/core/Base/en.locale.php
@@ -1,53 +1,69 @@
 <?php
 
+$LOCALE =& $GLOBALS
+           ['locale']
+           ['class']
+           ['core']
+           ['Base'];
+
 /** [__construct] **/
 /* an error occured when constructing an object */
-$GLOBALS['locale']['class']['core']['Base']['__construct']['error'] = 'Sorry, an error occured: cannot construct an object';
+$LOCALE['__construct']['error'] = 'Sorry, an error occured: cannot 
+construct an object';
 /** [/__construct] **/
 
 /** [clone] **/
 /* an error occured when cloning an object */
-$GLOBALS['locale']['class']['core']['Base']['clone']['error'] = 'Sorry, an error occured: cannot clone an object';
+$LOCALE['clone']['error'] = 'Sorry, an error occured: cannot clone an 
+object';
 /** [/clone] **/
 
 /** [display] **/
 /* an error occured when displaying an attribute */
-$GLOBALS['locale']['class']['core']['Base']['display']['error'] = 'Sorry, an element will not be displayed because of an error';
+$LOCALE['display']['error'] = 'Sorry, an element will not be displayed 
+because of an error';
 /** [/display] **/
 
 /** [get] **/
 /* an error occured when getting an attribute */
-$GLOBALS['locale']['class']['core']['Base']['get']['error'] = 'Sorry, an error occured: cannot get an attribute';
+$LOCALE['get']['error'] = 'Sorry, an error occured: cannot get an 
+attribute';
 /** [/get] **/
 
 /** [getDisp] **/
 /* an error occured when getting the displayer name */
-$GLOBALS['locale']['class']['core']['Base']['getDisp']['error'] = 'Sorry, an error occured: cannot generate a displayer name';
+$LOCALE['getDisp']['error'] = 'Sorry, an error occured: cannot generate 
+a displayer name';
 /** [/getDisp] **/
 
 /** [getGet] **/
 /* an error occured when getting the getter name */
-$GLOBALS['locale']['class']['core']['Base']['getGet']['error'] = 'Sorry, an error occured: cannot generate a getter name';
+$LOCALE['getGet']['error'] = 'Sorry, an error occured: cannot generate 
+a getter name';
 /** [/getGet] **/
 
 /** [getSet] **/
 /* an error occured when getting the setter name */
-$GLOBALS['locale']['class']['core']['Base']['getSet']['error'] = 'Sorry, an error occured: cannot generate a setter name';
+$LOCALE['getSet']['error'] = 'Sorry, an error occured: cannot generate 
+a setter name';
 /** [/getSet] **/
 
 /** [hydrate] **/
 /* an error occured when hydrating the object */
-$GLOBALS['locale']['class']['core']['Base']['hydrate']['error'] = 'Sorry, an error occured: cannot set values to an object';
+$LOCALE['hydrate']['error'] = 'Sorry, an error occured: cannot set 
+values to an object';
 /** [/hydrate] */
 
 /** [set] **/
 /* an error occured when setting a value to an attribute */
-$GLOBALS['locale']['class']['core']['Base']['set']['error'] = 'Sorry, an error occured: cannot set an attribute value';
+$LOCALE['set']['error'] = 'Sorry, an error occured: cannot set an 
+attribute value';
 /** [/set] **/
 
 /** [table] **/
 /* an error occured when converting the object */
-$GLOBALS['locale']['class']['core']['Base']['table']['error'] = 'Sorry, an error occured: cannot convert an object in array';
+$LOCALE['table']['error'] = 'Sorry, an error occured: cannot convert an 
+object in array';
 /** [/table] **/
 
 ?>
