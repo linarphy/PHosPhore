1f5c1d4bd9fc9e4b7c3988cc4228d21264e0a8fa
linarphy <linarphy@courrier.dev>
2023-03-10 00:19:20 +0100

    Update formatting in core\Logger

--- src/class/core/Logger.class.php
+++ src/class/core/Logger.class.php
@@ -42,7 +42,12 @@ class Logger
 
 		/* manage content */
 
-		$tokens = \preg_split('/({(?:\\}|[^\\}])+})/Um', $message, -1, PREG_SPLIT_DELIM_CAPTURE);
+		$tokens = \preg_split(
+			'/({(?:\\}|[^\\}])+})/Um',
+			$message,
+			-1,
+			PREG_SPLIT_DELIM_CAPTURE,
+		);
 
 		if (\count($substitution) > 0)
 		{
@@ -50,7 +55,9 @@ class Logger
 			{
 				if (\in_array('{' . $name . '}', $tokens))
 				{
-					foreach (\array_keys($tokens, '{' . $name . '}') as $key)
+					foreach (
+						\array_keys($tokens, '{' . $name . '}') as $key
+					)
 					{
 						$tokens[$key] = \phosphore_display($value);
 					}
@@ -60,12 +67,20 @@ class Logger
 
 		$message = \implode($tokens);
 
-		$tokens = \preg_split('/({(?:\\}|[^\\}])+})/Um', $config['format'], -1, PREG_SPLIT_DELIM_CAPTURE);
+		$tokens = \preg_split(
+			'/({(?:\\}|[^\\}])+})/Um',
+			$config['format'],
+			-1,
+			PREG_SPLIT_DELIM_CAPTURE,
+		);
 
 		if ($backtrace === null)
 		{
 			$backtrace = \debug_backtrace();
-			$key = \array_search(__FUNCTION__, \array_column($backtrace, 'function'));
+			$key = \array_search(
+				__FUNCTION__,
+				\array_column($backtrace, 'function'),
+			);
 			$backtrace = $backtrace[$key];
 		}
 
@@ -109,34 +124,64 @@ class Logger
 
 		foreach ($types as $type)
 		{
-			if ( $config['logged_types'] === '*' || \in_array($type, $config['logged_types']))
+			if ( 
+				$config['logged_types'] === '*' ||
+				\in_array($type, $config['logged_types'])
+			)
 			{
 				if (!\is_dir($config['directory']))
 				{
 					if (!\mkdir($config['directory'], 0774))
 					{
-						throw new \Exception('cannot create the directory ' . $config['directory'] . '. Please check permissions');
+						throw new \Exception(
+							'cannot create the directory ' .
+							$config['directory'] .
+							'. Please check permissions'
+						);
 					}
 				}
 
-				$directory = $config['directory'] . DIRECTORY_SEPARATOR . $type;
+				$directory = $config['directory'] .
+				             DIRECTORY_SEPARATOR .
+				             $type;
+
 				if (!\is_dir($directory))
 				{
 					if (!\mkdir($directory, 0774))
 					{
-						throw new \Exception('cannot create the directory ' . $directory . '. Please check permissions');
+						throw new \Exception(
+							'cannot create the directory ' .
+							$directory .
+							'. Please check permissions'
+						);
 					}
 				}
 
-				$file = \fopen($directory . DIRECTORY_SEPARATOR . 'log.txt', 'a');
+				$file = \fopen(
+					$directory . DIRECTORY_SEPARATOR . 'log.txt',
+					'a',
+				);
+
 				if (!$file)
 				{
-					throw new \Exception('cannot create the log file ' . $directory . DIRECTORY_SEPARATOR . 'log.txt');
+					throw new \Exception(
+						'cannot create the log file ' .
+						$directory .
+						DIRECTORY_SEPARATOR .
+						'log.txt'
+					);
 				}
+
 				\fwrite($file, $content);
+
 				if (!\fclose($file))
 				{
-					throw new \Exception('cannot close the log file ' . $directory . DIRECTORY_SEPARATOR . 'log.txt');
+					throw new \Exception(
+						'cannot close the log file ' .
+						$directory .
+						DIRECTORY_SEPARATOR .
+						'log.txt'
+					);
 				}
 			}
 		}
